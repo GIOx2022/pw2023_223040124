22a2e7594025f4294dc360f56d7ec7b6ea693d24
GIOx2022 <119664509+GIOx2022@users.noreply.github.com>
2023-02-20 18:53:36 +0700

    Add files via upload

---
+++ pw2023_223040124/kuliah/pertemuan2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 2</title>
+</head>
+<body>
+    <h1>
+        <?php echo 'Hello, World!';?>
+    </h1>
+    <h3>
+        <?php echo 'Hari Jum\'at';?>
+    </h3>
+    <hr>
+
+    <?php
+    $nama_depan = "Subagiono";
+    $nama_belakang = "Prakoso "
+    ?>
+
+    <h2>
+    <?php echo "Hello, $nama_depan $nama_belakang!";?>
+    </h2>
+</body>
+</html>
---
+++ pw2023_223040124/tugas/tugas1/1a.php
@@ -0,0 +1,22 @@
+<?php 
+$V1='Topi';
+$V2='Bundar';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1a.php</title>
+</head>
+<body>
+    <h2>
+        <i>
+    <?php 
+        echo "\"" . $V1 . " saya " . $V2 . ", " . $V2 . " "   . $V1 . " saya.\"";
+    ?>
+        </i>
+    </h2>
+</body>
+</html>
